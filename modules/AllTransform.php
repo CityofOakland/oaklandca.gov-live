57657c6d85b80ad06097486cc6273229bc629f36
Tim Lu <tim@goatsocial.com>
2021-03-11 12:07:59 -0500

    Added alternative ranking system

--- modules/AllTransform.php
+++ modules/AllTransform.php
@@ -9,29 +9,34 @@ use Craft;
 
 /**
  * Custom module to help parse information sent/received from the Algolia Scout plugin.
+ *
+ * This module is used as a generic template for all entry types.
+ * 
  */
+
 class AllTransform extends TransformerAbstract
 {
     public function transform(Entry $entry)
     {
         return [
-            'title' => $entry->title,
-            'url' => ScoutModule::entryUrl($entry),
-            'section' => $entry->section->name,
-            'type' => $entry->type->handle,
-            'date' => ScoutModule::entryDate($entry),
-            'displayDate' => ScoutModule::entryPrettyDate($entry),
-            'summary' => ScoutModule::richTextSplit($entry->summary),
-            'leadIn' => $entry->leadIn,
-            'about' => ScoutModule::richTextSplit($entry->about),
-            'bio' => strip_tags($entry->bio),
-            'body' => !empty($entry->contentBuilder) ? ScoutModule::contentBuilder($entry->contentBuilder) : ScoutModule::richTextSplit($entry->body),
-            'ctaButtonText' => ScoutModule::ctaButtonText($entry),
-            'groupHeadBio' => strip_tags($entry->groupHeadBio),
-            'groupHeadName' => $entry->groupHeadName,
-            'groupHeadTitle' => $entry->groupHeadTitle,
-            'mediaContact' => $entry->mediaContact,
-            'viewCount' => $entry->viewCount,
+            'about'             => ScoutModule::richTextSplit($entry->about),
+            'bio'               => strip_tags($entry->bio),
+            'body'              => !empty($entry->contentBuilder) ? ScoutModule::contentBuilder($entry->contentBuilder) : ScoutModule::richTextSplit($entry->body),
+            'ctaButtonText'     => ScoutModule::ctaButtonText($entry),
+            'date'              => ScoutModule::entryDate($entry),
+            'displayDate'       => ScoutModule::entryPrettyDate($entry),
+            'groupHeadBio'      => strip_tags($entry->groupHeadBio),
+            'groupHeadName'     => $entry->groupHeadName,
+            'groupHeadTitle'    => $entry->groupHeadTitle,
+            'leadIn'            => $entry->leadIn,
+            'mediaContact'      => $entry->mediaContact,
+            'priority'          => isset($entry->priority) ? ($entry->priority ? $entry->priority : 100) : 100,
+            'section'           => $entry->section->name,
+            'summary'           => ScoutModule::richTextSplit($entry->summary),
+            'title'             => $entry->title,
+            'type'              => $entry->type->handle,
+            'url'               => ScoutModule::entryUrl($entry),
+            'viewCount'         => $entry->viewCount,
         ];
     }
 }
