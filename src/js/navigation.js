25e74128758b903c14bc7ed763daa3d03502269b
Tim Lu <tim@goatsocial.com>
2022-03-28 13:28:31 -0400

    fix: reversed reverted changes

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,84 +1,178 @@
-var menuItems = document.querySelectorAll('li.has-submenu');
+//Navigation
+
+var menuItems = document.querySelectorAll('.has-submenu, #mobile-navigation');
+var menuItemsHover = document.querySelectorAll('.has-submenu:not(.click)');
+var menuItemsClick = document.querySelectorAll('.has-submenu.click > a, #mobile-navigation > a');
+
+function closeOtherMenuItems(parent){
+  Array.prototype.forEach.call(menuItemsClick, function(el, i){
+    if(parent){
+      if(el !== document.querySelector(parent)){
+        el.parentElement.classList.remove('open');
+        el.setAttribute('aria-expanded', "false");
+
+        if(el.getAttribute('data-affects') && el.getAttribute('data-affects-class')){
+          document.getElementById(el.getAttribute('data-affects')).classList.remove(el.getAttribute('data-affects-class'));
+        }
+      }
+    } else {
+      el.parentElement.classList.remove('open');
+      el.setAttribute('aria-expanded', "false");
+
+      if(el.getAttribute('data-affects') && el.getAttribute('data-affects-class')){
+        document.getElementById(el.getAttribute('data-affects')).classList.remove(el.getAttribute('data-affects-class'));
+      }
+    }
+  })
+}
+
+function toggleOverlay(){
+  console.log(document.querySelectorAll('#topbar .open').length);
+  if(document.querySelectorAll('#topbar .open').length > 0) {
+    document.querySelector('#topbar').classList.add('state-active');
+  } else {
+    document.querySelector('#topbar').classList.remove('state-active');
+  }
+}
+
+// Click
+Array.prototype.forEach.call(menuItemsClick, function(el, i){
+    el.addEventListener("mousedown", function(event){
+      if(this.parentElement.classList.contains('open')){
+        this.parentElement.classList.remove('open');
+        el.setAttribute('aria-expanded', "false");
+
+        if(el.getAttribute('data-affects') && el.getAttribute('data-affects-class')){
+          document.getElementById(el.getAttribute('data-affects')).classList.remove(el.getAttribute('data-affects-class'));
+        }
+
+      } else {
+        closeOtherMenuItems(el.getAttribute('data-parent'));
+        this.parentElement.classList.add('open');
+        el.setAttribute('aria-expanded', "true");
+
+        if(el.getAttribute('data-affects') && el.getAttribute('data-affects-class')){
+          document.getElementById(el.getAttribute('data-affects')).classList.add(el.getAttribute('data-affects-class'));
+        }
+      }
+      toggleOverlay();
+    });
+});
 
 // Hover
-Array.prototype.forEach.call(menuItems, function (el, i) {
-  el.addEventListener("mouseover", function (event) {
-    this.classList.add("open");
-    clearTimeout(timer);
-  });
-  el.addEventListener("mouseout", function (event) {
-    timer = setTimeout(function (event) {
-      el.classList.remove("open");
-    }, 500);
-  });
+Array.prototype.forEach.call(menuItemsHover, function(el, i){
+    el.addEventListener("mouseover", function(event){
+        this.className = "has-submenu open";
+        toggleOverlay();
+    });
+    el.addEventListener("mouseout", function(event){
+        document.querySelector(".has-submenu.open").className = "has-submenu";
+        toggleOverlay();
+    });
 });
 
 // Keyboard
-Array.prototype.forEach.call(menuItems, function (el, i) {
-  var trigger = el.querySelector('a');
-  var triggers = Array.prototype.slice.call(el.querySelectorAll('a.card'));
+Array.prototype.forEach.call(menuItems, function(el, i){
+    var trigger     = el.querySelector('a[aria-expanded]');
+    var triggers    = Array.prototype.slice.call(el.querySelectorAll('a.card, a.button, a.focusable, input, button'));
 
-  Array.prototype.forEach.call(triggers, function (card, i) {
-    card.addEventListener('blur', function (event) {
-      if (triggers.indexOf(event.relatedTarget) == -1) {
-        trigger.setAttribute('aria-expanded', "false");
-      }
+    Array.prototype.forEach.call(triggers, function(innerTrigger, i){
+        innerTrigger.addEventListener('blur', function(event){
+            if(triggers.indexOf(event.relatedTarget) == -1){
+                el.classList.remove('open');
+                trigger.setAttribute('aria-expanded', "false");
+
+            }
+        });
+    })
+
+    el.addEventListener('keydown', function (event) {
+        // var focused         = el.querySelector(':focus');
+        var target          = event.target;
+        var key             = event.which.toString();
+        var ctrlModifier    = (event.ctrlKey && key.match(/33|34/));
+
+        // Press Enter
+        if(key == "13"){
+          if(trigger.getAttribute("aria-expanded") == "true") {
+            el.classList.remove('open');
+            trigger.setAttribute('aria-expanded', "false");
+          } else {
+            trigger.setAttribute('aria-expanded', "true");
+            triggers[0].focus();
+
+            if(triggers[0].tagName == 'INPUT') {
+              event.preventDefault();
+            }
+          }
+          event.stopPropagation();
+        }
+
+        if(trigger.getAttribute('aria-expanded') == "true"){
+          if (key.match(/38|40/) || ctrlModifier) {
+              var index       = triggers.indexOf(target);
+              var direction   = (key.match(/34|40/)) ? 1 : -1;
+              var length      = triggers.length;
+              var newIndex    = (index + length + direction) % length;
+
+              if(index == -1) {
+                  triggers[0].focus();
+              } else if(direction == 1 && newIndex == 0) {
+                  el.classList.remove('open');
+                  trigger.setAttribute('aria-expanded', "false");
+                  trigger.focus();
+              } else if(direction == -1 && newIndex == (length - 1)) {
+                  el.classList.remove('open');
+                  trigger.setAttribute('aria-expanded', "false");
+                  trigger.focus();
+              } else {            
+                  if(isVisible(triggers[newIndex])){
+                    triggers[newIndex].focus();
+                    event.stopPropagation();
+                  } else {
+                    if(direction == 1) {
+                      for(var i = newIndex; i < triggers.length; i++){
+                        if(isVisible(triggers[i])){
+                          triggers[i].focus();
+                          break;
+                        }
+
+                        if(i == triggers.length - 1) {
+                          el.classList.remove('open');
+                          trigger.setAttribute('aria-expanded', "false");
+                          trigger.focus();
+                        }
+                      }
+                    } else {
+                      for(var i = newIndex; i >= 0; i--){
+                        if(isVisible(triggers[i])){
+                          triggers[i].focus();
+                          break;
+                        }
+
+                        if(i == 0) {
+                          el.classList.remove('open');
+                          trigger.setAttribute('aria-expanded', "false");
+                          trigger.focus();
+                        }
+                      }
+                    }
+                  }
+              }
+              event.preventDefault();
+          } else if (key.match(/35|36/)) {
+              switch (key) {
+                case '36':
+                  triggers[0].focus();
+                  break;
+                case '35':
+                  triggers[triggers.length - 1].focus();
+                  break;
+              }
+              event.preventDefault();
+          }
+        }
     });
-  })
-
-  el.addEventListener('keydown', function (event) {
-    var focused = el.querySelector(':focus');
-    var target = event.target;
-    var key = event.which.toString();
-    var ctrlModifier = (event.ctrlKey && key.match(/33|34/));
-
-    // Press Enter
-    if (key == "13") {
-      if (trigger.getAttribute("aria-expanded") == "true") {
-        trigger.setAttribute('aria-expanded', "false");
-      } else {
-        trigger.setAttribute('aria-expanded', "true");
-        triggers[0].focus();
-      }
-    }
-
-    if (key.match(/38|40/) || ctrlModifier) {
-      var index = triggers.indexOf(target);
-
-      var direction = (key.match(/34|40/)) ? 1 : -1;
-      var length = triggers.length;
-      var newIndex = (index + length + direction) % length;
-
-      if (index == -1) {
-        triggers[0].focus();
-      } else if (direction == 1 && newIndex == 0) {
-        trigger.setAttribute('aria-expanded', "false");
-        trigger.focus();
-      } else if (direction == -1 && newIndex == (length - 1)) {
-        trigger.setAttribute('aria-expanded', "false");
-        trigger.focus();
-      } else {
-        triggers[newIndex].focus();
-      }
-
-      event.preventDefault();
-
-    } else if (key.match(/35|36/)) {
-      // 35 = End, 36 = Home keyboard operations
-      switch (key) {
-        // Go to first accordion
-        case '36':
-          triggers[0].focus();
-          break;
-        // Go to last accordion
-        case '35':
-          triggers[triggers.length - 1].focus();
-          break;
-      }
-      event.preventDefault();
-
-    }
-  });
 })
 
 // Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
