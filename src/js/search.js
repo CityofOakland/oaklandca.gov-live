0fe796a5d09891b66151b0ce63ba99077ecd92a4
Christopher Kennedy <chris@onebrightlight.com>
2022-04-05 02:48:11 -0700

    feat(a11y): :construction: Add basic render function for custom refinement

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,4 @@
+// Instantiate Algolia
 const allSearch = instantsearch({
   appId: "6V5VJO8ZG2",
   apiKey: "9bded46d3070b2089499c70b2389708b",
@@ -14,6 +15,7 @@ const allSearch = instantsearch({
   },
 });
 
+// Instantiate the search input box
 allSearch.addWidget(
   instantsearch.widgets.searchBox({
     container: "#search-input",
@@ -21,6 +23,7 @@ allSearch.addWidget(
   })
 );
 
+// Instantiate the refinement clear button
 allSearch.addWidget(
   instantsearch.widgets.clearAll({
     container: '#clear-refinements',
@@ -29,87 +32,134 @@ allSearch.addWidget(
     }  })
 );
 
-// 1. Create a render function
-const renderPagination = (renderOptions, isFirstRender) => {
+/*
+  Create a custom pagination widget
+  This is used to create a custom pagination widget that is used in the search results page
+*/
+  // 1. Create a render function for the pagination widget
+  const renderPagination = (renderOptions, isFirstRender) => {
 
-  const container = document.querySelector('#bottom-pagination');
+    const container = document.querySelector('#bottom-pagination');
 
-  const { pages, currentRefinement, nbPages, refine } = renderOptions;
+    const { pages, currentRefinement, nbPages, refine } = renderOptions;
 
-  if(nbPages > 1 && nbPages <= 7) {
+    if(nbPages > 1 && nbPages <= 7) {
 
-    const previous  = `<li><a class="prev ${currentRefinement > 0 ? '' : 'disabled' }" href="#" data-value="${currentRefinement - 1}">Previous</a></li>`;
-    const next      = `<li><a class="next ${currentRefinement < nbPages - 1 ? '' : 'disabled'}" href="#" data-value="${currentRefinement + 1}">Next</a></li>`;
+      const previous  = `<li><a class="prev ${currentRefinement > 0 ? '' : 'disabled' }" href="#" data-value="${currentRefinement - 1}">Previous</a></li>`;
+      const next      = `<li><a class="next ${currentRefinement < nbPages - 1 ? '' : 'disabled'}" href="#" data-value="${currentRefinement + 1}">Next</a></li>`;
 
-    const pages = [...Array(nbPages).keys()];;
+      const pages = [...Array(nbPages).keys()];;
 
-    container.innerHTML = `
-      <ul class="pagination">
-        ${previous}
-        ${pages
-          .map(page => `<li><a class="page ${currentRefinement === page ? 'page-active' : ''}" href="#" data-value="${page}">${page + 1}</a></li>`)
-          .join('')}
-        ${next}
-      </ul>
-    `;
+      container.innerHTML = `
+        <ul class="pagination">
+          ${previous}
+          ${pages
+            .map(page => `<li><a class="page ${currentRefinement === page ? 'page-active' : ''}" href="#" data-value="${page}">${page + 1}</a></li>`)
+            .join('')}
+          ${next}
+        </ul>
+      `;
 
-    [...container.querySelectorAll('a')].forEach(element => {
-      element.addEventListener('click', event => {
-        event.preventDefault();
-        refine(event.currentTarget.dataset.value);
+      [...container.querySelectorAll('a')].forEach(element => {
+        element.addEventListener('click', event => {
+          event.preventDefault();
+          refine(event.currentTarget.dataset.value);
+        });
       });
-    });
-  } else if(nbPages > 1 && nbPages > 7) {
-    const previous  = `<li><a class="prev ${currentRefinement > 0 ? '' : 'disabled' }" href="#" data-value="${currentRefinement - 1}">Previous</a></li>`;
-    const next      = `<li><a class="next ${currentRefinement < nbPages - 1 ? '' : 'disabled'}" href="#" data-value="${currentRefinement + 1}">Next</a></li>`;
+    } else if(nbPages > 1 && nbPages > 7) {
+      const previous  = `<li><a class="prev ${currentRefinement > 0 ? '' : 'disabled' }" href="#" data-value="${currentRefinement - 1}">Previous</a></li>`;
+      const next      = `<li><a class="next ${currentRefinement < nbPages - 1 ? '' : 'disabled'}" href="#" data-value="${currentRefinement + 1}">Next</a></li>`;
 
-    const first = pages.includes(0) ? '' : `<li><a class="page" href="#" data-value="0">1</a></li><li>...</li>`;
-    const last  = pages.includes(nbPages - 1) ? '' : `<li>...</li><li><a class="page" href="#" data-value="${nbPages - 1}">${nbPages}</a></li>`;
+      const first = pages.includes(0) ? '' : `<li><a class="page" href="#" data-value="0">1</a></li><li>...</li>`;
+      const last  = pages.includes(nbPages - 1) ? '' : `<li>...</li><li><a class="page" href="#" data-value="${nbPages - 1}">${nbPages}</a></li>`;
 
-    container.innerHTML = `
-      <ul class="pagination">
-        ${previous}
-        ${first}
-        ${pages
-          .map(page => `<li><a class="page ${currentRefinement === page ? 'page-active' : ''}" href="#" data-value="${page}">${page + 1}</a></li>`)
-          .join('')}
-        ${last}
-        ${next}
-      </ul>
-    `;
+      container.innerHTML = `
+        <ul class="pagination">
+          ${previous}
+          ${first}
+          ${pages
+            .map(page => `<li><a class="page ${currentRefinement === page ? 'page-active' : ''}" href="#" data-value="${page}">${page + 1}</a></li>`)
+            .join('')}
+          ${last}
+          ${next}
+        </ul>
+      `;
 
-    [...container.querySelectorAll('a')].forEach(element => {
-      element.addEventListener('click', event => {
-        event.preventDefault();
-        refine(event.currentTarget.dataset.value);
+      [...container.querySelectorAll('a')].forEach(element => {
+        element.addEventListener('click', event => {
+          event.preventDefault();
+          refine(event.currentTarget.dataset.value);
+        });
       });
-    });
-  } else {
-    container.innerHTML = '';
+    } else {
+      container.innerHTML = '';
+    }
+  };
+
+  // 2. Create the custom pagination widget
+  const customPagination = instantsearch.connectors.connectPagination(
+    renderPagination
+  );
+
+  // 3. Instantiate the pagination widget
+  allSearch.addWidgets([
+    customPagination({
+      padding: 2,
+    })
+  ]);
+
+// custom `renderFn` to render the custom RefinementList widget
+function renderFn(RefinementListRenderingOptions, isFirstRendering) {
+  if (isFirstRendering) {
+    RefinementListRenderingOptions.widgetParams.containerNode
+      .html('<fieldset></fieldset>')
   }
-};
 
-// 2. Create the custom widget
-const customPagination = instantsearch.connectors.connectPagination(
-  renderPagination
-);
+    RefinementListRenderingOptions.widgetParams.containerNode
+      .find('li[data-refine-value]')
+      .each(function() { $(this).off('click'); });
 
-// 3. Instantiate
-allSearch.addWidgets([
-  customPagination({
-    padding: 2,
-  })
-]);
+  if (RefinementListRenderingOptions.canRefine) {
+    var list = RefinementListRenderingOptions.items.map(function(item) {
+      return `
+        <li data-refine-value="${item.value}">
+          <input type="radio" name="Section" value="${item.value}" ${item.isRefined ? 'checked' : ''} />
+          <a href="${RefinementListRenderingOptions.createURL(item.value)}">
+            ${item.label} (${item.count})
+          </a>
+        </li>
+      `;
+    });
 
+    RefinementListRenderingOptions.widgetParams.containerNode.find('fieldset').html(list);
+    RefinementListRenderingOptions.widgetParams.containerNode
+      .find('li[data-refine-value]')
+      .each(function() {
+        $(this).on('click', function(event) {
+          event.stopPropagation();
+          event.preventDefault();
+
+          RefinementListRenderingOptions.refine($(this).data('refine-value'));
+        });
+      });
+  } else {
+    RefinementListRenderingOptions.widgetParams.containerNode.find('fieldset').html('');
+  }
+}
+
+// connect `renderFn` to RefinementList logic
+var customRefinementList = instantsearch.connectors.connectRefinementList(renderFn);
+
+// mount widget on the page
 allSearch.addWidget(
-  instantsearch.widgets.menu({
-    container: "#section-filter",
-    attributeName: "section",
-    operator: 'or',
+  customRefinementList({
+    containerNode: $('#section-filter'),
+    attributeName: 'section',
     limit: 10,
   })
-);    
-
+);
+    
+// Instantiate the results/hits
 allSearch.addWidget(
   instantsearch.widgets.hits({
     container: "#all-hits",
@@ -136,3 +186,9 @@ allSearch.start();
 
 const searchNav = Array.from(document.querySelectorAll('a[data-holder]'));
 
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log("HMR")
+  });
+}
