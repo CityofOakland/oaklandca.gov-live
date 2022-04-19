dac802d333be88186e1fd8b774cb9f1ab8d6d9b2
Christopher Kennedy <chris@onebrightlight.com>
2022-04-18 17:57:38 -0700

    ✨ (plugins): add sentry.io integration
    Adds sentry.io plugin to the mix, which allows us to track errors and receive notifications when they hit a certain threshold.

---
+++ config/craft-sentry.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'enabled'       => getenv('SENTRY_DSN') ?: false,
+    'anonymous'     => true,
+    'clientDsn'     => getenv('SENTRY_DSN') ?: null,
+    'excludedCodes' => ['400', '404', '429'],
+    'release'       => getenv('SENTRY_RELEASE') ?: null,
+];
