da0a4e74f90c7975ad12d709d3684d5e38f91526
Tim Lu <tim@goatsocial.com>
2022-05-20 13:08:35 -0700

    fix: events import work

--- plugins/dashboardconfiguration/src/console/controllers/DefaultController.php
+++ plugins/dashboardconfiguration/src/console/controllers/DefaultController.php
@@ -10,7 +10,7 @@ use craft\db\Table;
 use craft\fields\Matrix;
 use craft\helpers\Db;
 use craft\helpers\Queue;
-use goat\dashboardconfiguration\queue\jobs\CustomFindAndReplace;
+use goat\dashboardconfiguration\queue\jobs\EventsCalendarizeUpdate;
 
 use yii\console\Controller;
 use yii\helpers\Console;
@@ -39,16 +39,9 @@ class DefaultController extends Controller
 
     // TODO: Move this functionality to another plugin.
 
-    public function actionFindAndReplace()
+    public function actionEventsImport()
     {
-        // 1639761861_detailed_urls_mapped
-        // 1639768623_detailed_urls_mapped
-
-        // 1639769985_detailed_urls_mapped
-        // 1639774029_detailed_urls_mapped
-        // manual_detailed_urls_mapped
-
-        $data = file(__dir__ . "/manual_detailed_urls_mapped.txt");
+        $data = file(__dir__ . "/events.csv");
         $total_jobs = 0;
 
         foreach ($data as $index => $row) {
@@ -57,233 +50,22 @@ class DefaultController extends Controller
                 $row[$index] = trim($item);
             }
             $elementId  = $row[0];
-            $table      = $row[1];
-            $column     = $row[2];
-            $find       = $row[3];
-            $replace    = $row[4];
+            $startDate  = $row[1];
+            $endDate    = $row[2];
 
-            Queue::push(new CustomFindAndReplace([
-                'elementId' => $elementId,
-                'table'     => $table,
-                'column'    => $column,
-                'find'      => $find,
-                'replace'   => $replace,
+            Queue::push(new EventsCalendarizeUpdate([
+                'elementId'     => $elementId,
+                'startDate'     => $startDate,
+                'endDate'       => $endDate,
             ]));
 
-            // Queue::push(new CustomFindAndReplace([
-            //     'elementId' => $elementId,
-            //     'table'     => $table,
-            //     'column'    => $column,
-            //     'find'      => trim($find, '"'),
-            //     'replace'   => trim($replace, '"'),
-            // ]));
+            echo $elementId;
+            echo PHP_EOL;
 
             $total_jobs = $total_jobs + 1;
         }
 
+
         echo 'Total jobs queued: ' . $total_jobs;
-    }
-
-    public function actionFind()
-    {
-        $timestamp = time();
-        $export_urls_uri            = __dir__ . "/" . $timestamp . "_urls.txt";
-        $export_urls_context_uri    = __dir__ . "/" . $timestamp . "_urls_w_context.txt";
-        $export_urls_detailed_uri   = __dir__ . "/" . $timestamp . "_detailed_urls.txt";
-        file_put_contents($export_urls_uri, "");
-        file_put_contents($export_urls_detailed_uri, "");
-
-        $this->_textColumns = [
-            [Table::CONTENT, 'title'],
-        ];
-
-        foreach (Craft::$app->getFields()->getAllFields() as $field) {
-            if ($field instanceof Matrix) {
-                $this->_checkMatrixField($field);
-            } else {
-                $this->_checkField($field, Table::CONTENT, 'field_');
-            }
-        }
-
-        $count = 0;
-
-        $all_matches = [];
-
-        $this->_textColumns[] = ['stc_26_table', 'field_column1Cell'];
-        $this->_textColumns[] = ['stc_26_table', 'field_column2Cell'];
-        $this->_textColumns[] = ['stc_26_table', 'field_column3Cell'];
-        $this->_textColumns[] = ['stc_26_table', 'field_column4Cell'];
-
-        $this->_textColumns[] = ['stc_27_table', 'field_column1Cell'];
-        $this->_textColumns[] = ['stc_27_table', 'field_column2Cell'];
-        $this->_textColumns[] = ['stc_27_table', 'field_column3Cell'];
-
-        $this->_textColumns[] = ['stc_30_table', 'field_column1Cell'];
-        $this->_textColumns[] = ['stc_30_table', 'field_column2Cell'];
-
-        foreach ($this->_textColumns as $i => [$table, $column]) {
-
-            $rows = (new \yii\db\Query())
-                ->select(['elementId',$column])
-                ->from($table)
-                ->where(['like', $column, 'oaklandnet.com'])
-                ->all();
-
-            foreach($rows as $row) {
-                $elementId  = $row['elementId'];
-                $content    = $row[$column];
-                $edit_url   = '';
-
-                $element = Craft::$app->elements->getElementById($elementId);
-
-                // Figure out which entry this is coming from.
-                if($element){
-                    switch ($element->displayName()) {
-                        case 'Entry':
-                            $edit_url = $element->getCurrentRevision() ? $element->getCurrentRevision()->getCpEditUrl() : $element->getCpEditUrl();
-                            break;
-                        case 'Matrix Block':
-                            if($matrixblock = Craft::$app->matrix->getBlockById($elementId)){
-                                $entry = Craft::$app->elements->getElementById($matrixblock->ownerId);
-                                if($entry){
-                                    $edit_url   = $entry->getCurrentRevision() ? $entry->getCurrentRevision()->getCpEditUrl() : $entry->getCpEditUrl();
-                                }
-                            }
-                            break;
-                        case 'SuperTable Block':
-                            if($supertableblock = SuperTableBlockElement::find()->id($elementId)->one()){
-                                if($matrixblock = Craft::$app->elements->getElementById($supertableblock->ownerId)){
-                                    $entry = Craft::$app->elements->getElementById($matrixblock->ownerId);
-                                    if($entry){
-                                        $edit_url   = $entry->getCurrentRevision() ? $entry->getCurrentRevision()->getCpEditUrl() : $entry->getCpEditUrl();
-                                    }
-                                }
-                            }
-                        default:
-                            break;
-                    }
-                }
-
-                $edit_url = preg_replace('/\?.*/', '', $edit_url);
-                echo $edit_url;
-                echo PHP_EOL;
-
-                $regex = "/(https?:\/\/www2.oaklandnet\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/";
-
-                preg_match_all($regex, $content, $matches);
-
-                $matched_urls = [
-                    'id'        => $elementId,
-                    'table'     => $table,
-                    'column'    => $column,
-                    'edit_url'  => $edit_url,
-                    'matches'   => [],
-                ];
-
-                foreach($matches[0] as $value) {
-                    $count = $count + 1;
-                    echo $count;
-                    echo PHP_EOL;
-
-                    $matched_urls['matches'][$value] = $value;
-                }
-
-                if(count($matches[0])){
-                    $all_matches[] = $matched_urls;
-                }
-            }
-        }
-
-        $unique_urls = [];
-
-        foreach($all_matches as $matched_urls){
-            foreach($matched_urls['matches'] as $match) {
-                file_put_contents($export_urls_detailed_uri, $matched_urls['id'], FILE_APPEND);
-                file_put_contents($export_urls_detailed_uri, ", ", FILE_APPEND);
-                file_put_contents($export_urls_detailed_uri, $matched_urls['table'], FILE_APPEND);
-                file_put_contents($export_urls_detailed_uri, ", ", FILE_APPEND);
-                file_put_contents($export_urls_detailed_uri, $matched_urls['column'], FILE_APPEND);
-                file_put_contents($export_urls_detailed_uri, ", ", FILE_APPEND);
-                file_put_contents($export_urls_detailed_uri, $match, FILE_APPEND);
-                file_put_contents($export_urls_detailed_uri, "\n", FILE_APPEND);
-
-                $unique_urls[$match] = [$match, $matched_urls['edit_url']];
-            }
-        }
-
-        foreach($unique_urls as $unique_url) {
-            file_put_contents($export_urls_uri, $unique_url[0], FILE_APPEND);
-            file_put_contents($export_urls_uri, "\n", FILE_APPEND);
-        }
-
-        foreach($unique_urls as $unique_url) {
-            file_put_contents($export_urls_context_uri, $unique_url[0], FILE_APPEND);
-            file_put_contents($export_urls_context_uri, ", ", FILE_APPEND);
-            file_put_contents($export_urls_context_uri, $unique_url[1], FILE_APPEND);
-            file_put_contents($export_urls_context_uri, "\n", FILE_APPEND);
-        }
-
-        echo count($unique_urls);
-    }
-
-    /**
-     * Checks whether the given field is saving data into a textual column, and saves it accordingly.
-     *
-     * @param FieldInterface $field
-     * @param string $table
-     * @param string $fieldColumnPrefix
-     */
-    private function _checkField(FieldInterface $field, string $table, string $fieldColumnPrefix)
-    {
-        if (!$field::hasContentColumn()) {
-            return;
-        }
-
-        $columnType = $field->getContentColumnType();
-        if (is_array($columnType)) {
-            $columnType = reset($columnType);
-        }
-
-        if (!preg_match('/^\w+/', $columnType, $matches)) {
-            return;
-        }
-
-        $columnType = strtolower($matches[0]);
-
-        if (in_array($columnType, [
-            'tinytext',
-            'mediumtext',
-            'longtext',
-            'text',
-            'varchar',
-            'string',
-            'char',
-        ], true)) {
-
-            if($field->columnSuffix){
-                $this->_textColumns[] = [$table, $fieldColumnPrefix . $field->handle . '_' . $field->columnSuffix];
-            } else {
-                $this->_textColumns[] = [$table, $fieldColumnPrefix . $field->handle];
-            }
-        }
-    }
-
-    /**
-     * Registers any textual columns associated with the given Matrix field.
-     *
-     * @param Matrix $matrixField
-     * @throws Exception if the content table can't be determined
-     */
-    private function _checkMatrixField(Matrix $matrixField)
-    {
-        $blockTypes = Craft::$app->getMatrix()->getBlockTypesByFieldId($matrixField->id);
-
-        foreach ($blockTypes as $blockType) {
-            $fieldColumnPrefix = 'field_' . $blockType->handle . '_';
-
-            foreach ($blockType->getFields() as $field) {
-                $this->_checkField($field, $matrixField->contentTable, $fieldColumnPrefix);
-            }
-        }
     }
 }
---
+++ plugins/dashboardconfiguration/src/queue/jobs/EventsCalendarizeUpdate.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace goat\dashboardconfiguration\queue\jobs;
+
+use Craft;
+use craft\base\FieldInterface;
+use craft\db\Table;
+use craft\fields\Matrix;
+use craft\helpers\Db;
+use craft\helpers\ElementHelper;
+use craft\queue\BaseJob;
+use yii\base\Exception;
+
+/**
+ * FindAndReplace job
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.0.0
+ */
+class EventsCalendarizeUpdate extends BaseJob
+{
+
+    public $elementId;
+    public $startDate;
+    public $endDate;
+
+    /**
+     * @inheritdoc
+     */
+    public function execute($queue)
+    {
+        Db::update(
+            '{{%calendarize}}', 
+            [
+                'startDate' => $startDate,
+                'endDate'   => $endDate
+            ],
+            'ownerId='.$elementId
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultDescription(): string
+    {
+        return Craft::t('app', 'Updating Calendarize row.');
+    }
+}
