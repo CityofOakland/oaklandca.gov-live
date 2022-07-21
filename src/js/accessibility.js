f34526a5285b5be53359d8f8dc2bd3a08cbbee47
Tim Lu <tim@goatsocial.com>
2022-07-21 12:31:50 -0700

    fix: external link icon

--- src/js/accessibility.js
+++ src/js/accessibility.js
@@ -1,15 +1,17 @@
 import jQuery from 'jquery'
 
 function updateExternalLinks($){
-  var $links = $('main a[target="_blank"]');
+  var $links = $('.components a[target="_blank"]');
   if($links.length > 0) {
     $links.each(function(index, element){
       var $this = $(element);
       $this.attr('aria-label', $this.text() + ' (open in new tab)');
 
-      // if($this.find('img').length == 0 && $this.find('svg').length == 0){
-      //   $this.addClass('external-link');
-      // }
+      if(!$this.hasClass('.btn')){
+        if($this.find('img').length == 0 && $this.find('svg').length == 0){
+          $this.addClass('external-link');
+        }
+      }
     });
   }
 }
