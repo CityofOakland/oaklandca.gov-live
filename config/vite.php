e473f971c95c107662fc6984abea82e81a94b079
Christopher Kennedy-Nuñez <chris@onebrightlight.com>
2023-02-10 16:20:18 -0500

    🔨 (vite) adjust vite url values to env variable only

--- config/vite.php
+++ config/vite.php
@@ -5,7 +5,7 @@ use craft\helpers\App;
 return [
 		'checkDevServer' => true,
 		'devServerInternal' => 'http://localhost:3000',
-		'devServerPublic' => App::env('CRAFT_VITE_URL') . ':3000',
+		'devServerPublic' => App::env('VITE_URL'),
 		'serverPublic' => '/dist/',
 		'useDevServer' => App::env('ENVIRONMENT') === 'dev',
 		'manifestPath' => '@webroot/dist/manifest.json',
