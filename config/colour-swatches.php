56e5d49654d203f815fbe1087f9f27e6fff61f42
Christopher Kennedy-Nuñez <chris@onebrightlight.com>
2023-03-10 16:14:09 -0800

    🐛 (content builder) QA icon cards
    - Add Font Awesome 6 library (note: this isn't available as a CDN feature in the plugin, so we have to manually add the icons… all 6000 of them)
    - Update card declaration to use aria-label to say if the link is going to an external page.
    - Removed final card bottom margin

---
+++ config/colour-swatches.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * color-swatches plugin for Craft CMS 3.x.
+ *
+ * Let clients choose from a predefined set of colours.
+ *
+ * @link      https://percipio.london
+ *
+ * @copyright Copyright (c) 2020 Percipio.London
+ */
+
+return [
+
+	// Custom  palettes, fixed options [label, default (boolean), colour (array(colour, customOptions)) ]
+		'palettes' => [
+				'Cards' => [  // custom label
+						[
+								'label'   => 'Dark Green',
+								'default' => true,
+								'color'   =>  [
+										[
+												'color'             => '#207127',
+												'background'        => 'bg-green-800',
+												'backgroundHover'   => 'hover:bg-green-600',
+												'text'              => 'text-white',
+												'textHover'         => 'hover:text-white'
+										]
+								]
+						],
+						[
+								'label'   => 'White',
+								'default' => false,
+								'color'   =>  [
+										[
+												'color'             => '#ffffff',
+												'background'        => 'bg-white',
+												'backgroundHover'   => 'hover:bg-gray-100',
+												'text'              => 'text-gray-1000',
+												'textHover'         => 'hover:text-gray-1000'
+										]
+								]
+						],
+				]
+		]
+
+];
